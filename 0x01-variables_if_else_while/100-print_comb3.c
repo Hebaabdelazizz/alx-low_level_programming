b7d330c4652413e0715badb07b5a1fd118f6fff0
Hebaabdelazizzz <hebauwk25800@gmail.com>
2023-02-17 21:59:28 +0200

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,19 +13,19 @@ int main(void)
 	int d, p;
 
 	for (d = '0'; d < '9'; d++)
-	{	
-	for (p = d + 1; p <= '9'; p++)
-	{
-	if (p != d)
-	{
-	putchar(d);
-	putchar(p);
-       	if (d == '8' && p == '9')
-	continue;
-	putchar(',');
-	putchar(' ');
-	}	
-	}
+		{	
+		for (p = d + 1; p <= '9'; p++)
+			{
+			if (p != d)
+				{	
+				putchar(d);
+				putchar(p);
+       				if (d == '8' && p == '9')
+				continue;
+				putchar(',');
+				putchar(' ');
+			}		
+		}
 	}
 	putchar('\n');
 	return (0);
