c6c1f2cb44784ed7692259e3074841441fab2003
Hebaabdelazizzz <hebauwk25800@gmail.com>
2023-02-17 21:20:51 +0200

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,10 +7,10 @@
  * Return: Always 0 (success)
  */
 int main(void)
-        {
+{
 	int a;
 
-	for ( a = 0; a < 10; a++)
+	for (a = 0; a < 10; a++)
 	{
 		putchar(a + '0');
 		if (a < 9)
@@ -21,4 +21,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-	}
+}
