fd8ba34de70070cd69b7dbc46e259fa6c141cde0
heba abdelaziz <hebauwk25800@gmail.com>
2023-03-01 17:36:24 +0200

    Create 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ * @n: the integer to be printed
+ */
+void print_number(int n)
+{
+	unsigned int x;
+
+	x = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		x = -n;
+	}
+	if (n / 10)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
+}
